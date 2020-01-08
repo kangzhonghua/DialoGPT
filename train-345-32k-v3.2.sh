c08905eb9efa7056addaa4c1804451e5c3e9ba47
kangzhonghua <41819727@qq.com>
2020-01-08 15:20:02 +0800

    调整了参数

--- train-345-32k-v3.2.sh
+++ train-345-32k-v3.2.sh
@@ -1,6 +1,10 @@
-python  LSP_train-bpe-cn.py \
-    --train_input_file /home/Public/data/transfer-learning/output/output-qa/xinli001_jiandanxinli-qa.topics_去重_DialogGPT-GPT2BPETokenizer_CN_32K_v3_2_BPE_db \
-    --eval_input_file /home/Public/data/transfer-learning/output/output-qa/xinli001_jiandanxinli-qa.topics_去重_DialogGPT/valid_0_DialogGPT.tsv \
+#!/bin/bash
+#export OMP_NUM_THREADS=8
+python \
+    -m torch.distributed.launch --nproc_per_node=2 \
+     LSP_train-bpe-cn.py \
+    --train_input_file ./xinli_qa_data/train_db \
+    --eval_input_file ./xinli_qa_data/valid_0_DialogGPT.tsv \
     --output_dir ./output_model/345m-hmwebmix-bpe-v3.2 \
     --model_name_or_path ./pre-train-345m-hmwebmix-bpe-v3.2 \
     --init_checkpoint ./pre-train-345m-hmwebmix-bpe-v3.2/pytorch_model.bin \
@@ -8,4 +12,6 @@ python  LSP_train-bpe-cn.py \
     --train_batch_size 4 \
     --eval_batch_size 4 \
     --no_token_id False \
-    --valid_step 100
+    --max_seq_length 512 \
+    --gradient_accumulation_steps 4 \
+    --valid_step 10000 \
